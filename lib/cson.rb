d6b697dd91a9af6d3d217642f17ebbcad39bd3d3
Alex Miller <apmiller@covermymeds.com>
2021-02-22 21:01:05 -0500

    Adds type and folder attributes

--- lib/cson.rb
+++ lib/cson.rb
@@ -1,14 +1,19 @@
 require 'date'
+require 'pathname'
+require 'json'
 
 class CSON
-  attr_reader :contents
+  attr_reader :contents, :file
 
   PATTERNS = {
     created_at: 'createdAt:\s\"(?<created_at>.+)\"\n',
-    updated_at: 'updatedAt:\s\"(?<updated_at>.+)\"\n'
+    updated_at: 'updatedAt:\s\"(?<updated_at>.+)\"\n',
+    type:       'type:\s\"(?<type>.+)\"\n',
+    folder_key: 'folder:\s\"(?<folder_key>.+)\"\n'
   }.freeze
 
   def initialize(file)
+    @file = file
     @contents = file.read
   end
 
@@ -20,11 +25,37 @@ class CSON
     DateTime.parse(document_map[:updated_at])
   end
 
+  def type
+    document_map[:type]
+  end
+
+  def folder
+    folder_data['name']
+  end
+
   private
 
   def document_map
     @document_map = PATTERNS.each_with_object({}) do |(key, pattern), hash|
       hash[key] = contents.match(pattern)[key]
     end
+  end
+
+  def boostnote_json
+    @boostnote_json = JSON.parse(
+      File.read(file_pathname.dirname.parent.join('boostnote.json'))
+    )
+  end
+
+  def file_pathname
+    Pathname.new(file.path)
+  end
+
+  def folder_key
+    document_map[:folder_key]
+  end
+
+  def folder_data
+    boostnote_json['folders'].find { |folder| folder['key'] == folder_key }
   end
 end
--- spec/cson_spec.rb
+++ spec/cson_spec.rb
@@ -1,13 +1,14 @@
 require 'cson'
 
 RSpec.describe CSON do
-  let(:file_path) { 'spec/test_cson.cson' }
+  let(:note_path) { 'spec/fixtures/notes/example_note.cson' }
+  let(:boostnote_json) { JSON.parse(File.read('spec/fixtures/boostnote.json')) }
 
-  subject { described_class.new(File.new(file_path)) }
+  subject { described_class.new(File.new(note_path)) }
 
   describe '#initialize' do
     it 'sets the context of the file' do
-      expect(subject.contents).to eq File.read(file_path)
+      expect(subject.contents).to eq File.read(note_path)
     end
   end
 
@@ -20,6 +21,18 @@ RSpec.describe CSON do
   describe '#updated_at' do
     it 'returns the updatedAt as a DateTime object' do
       expect(subject.updated_at).to eq DateTime.new(2021, 2, 19, 16, 19, 59.940)
+    end
+  end
+
+  describe '#type' do
+    it 'returns the note type' do
+      expect(subject.type).to eq 'MARKDOWN_NOTE'
+    end
+  end
+
+  describe '#folder' do
+    it 'returns the folder name from the boostnote.json file' do
+      expect(subject.folder).to eq boostnote_json['folders'][0]['name']
     end
   end
 end
