c1f79a4d2392bfd4aaf030b67f24006bcc0e0c24
Alex Miller <apmiller@covermymeds.com>
2021-05-23 17:47:59 -0400

    Enable target registration

--- lib/boostnote_converter.rb
+++ lib/boostnote_converter.rb
@@ -3,19 +3,25 @@
 require_relative 'boostnote_converter/cson'
 require_relative 'boostnote_converter/pandoc'
 require_relative 'boostnote_converter/org'
+require_relative 'boostnote_converter/targets'
 require_relative 'boostnote_converter/writer'
 require_relative 'boostnote_converter/errors/content_conversion_failed_error'
 
 module BoostnoteConverter
-  TARGETS = {
-    org: Org
-  }.freeze
+  @targets = {}
 
-  # Usage: BoostnoteConverter.convert(source: 'spec/fixtures/notes/example_note.cson', target: :org, output_path: "/")
+  def self.register_target(target_name, target_class)
+    @targets = @targets.merge({ target_name.to_sym => target_class })
+  end
+
+  TARGETS.each do |target_name, target_class|
+    register_target(target_name, target_class)
+  end
+
   def self.convert(source:, target:, **opts)
     output_path = Pathname.new(opts.fetch(:output_path, Dir.pwd)).realpath
     path = Pathname.new(source).realpath
-    target_class = TARGETS[target]
+    target_class = @targets.fetch(target)
 
     paths = if path.directory?
               Dir.entries(path).map { |e| (path + e).realpath }.reject(&:directory?)
